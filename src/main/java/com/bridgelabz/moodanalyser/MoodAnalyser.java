3278e7cf417bad9f6156b7530d0eefa80151359d
777shetty <shetty.chetu7@gmail.com>
2021-09-29 23:28:24 +0530

    ADDED:Analysis and respond happ and sad messages

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.moodanalyser;
 
-public class MoodAnalyser {
+public class MoodAnalyser{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyser");
+	}
 
+	public String analyzeMood(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
